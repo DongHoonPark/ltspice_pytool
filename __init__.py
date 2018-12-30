d86d1c77f83935e5ea0689a9a8d6e449ae1043de
donghoonpark <donghun94@gmail.com>
2018-12-30 15:58:50 +0900

    pip release

---
+++ __init__.py
@@ -0,0 +1,2 @@
+name = 'ltspice'
+import ltspice
--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
-from setuptools import setup, find_packages
-
+from distutils.core import setup
 setup(
-    name             = 'ltspice',
-    version          = '0.1',
-    description      = 'ltspice python analysis tool',
-    author           = 'Donghoon Park',
-    author_email     = 'donghun94@gmail.com',
-    install_requires = [numpy],
-    packages         = find_packages(exclude = ['docs', 'tests*']),
-    keywords         = ['ltspice'],
-    python_requires  = '>=3',
-    package_data     =  {},
-    zip_safe=False,
-    classifiers      = [
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ]
+  name = 'ltspice',         # How you named your package folder (MyLib)
+  packages = ['ltspice'],   # Chose the same as "name"
+  version = '0.2.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'DESC',   # Give a short description about your library
+  author = 'DonghoonPark',                   # Type in your name
+  author_email = 'donghun94@snu.ac.kr',      # Type in your E-Mail
+  keywords = ['ltspice', 'multi point simulation'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+      ],
+  classifiers=[
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
